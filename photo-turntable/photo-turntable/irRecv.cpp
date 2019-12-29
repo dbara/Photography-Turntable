4dee7b41cd35d65f24d63036414827fe1b12a6e5
dbara <d.bara89@gmail.com>
2019-12-29 21:12:41 +0100

    Updated IRemote, autoSearch I2C Display
    Updated IRemote and added autoSearch for I2C Display

--- photo-turntable/photo-turntable/irRecv.cpp
+++ photo-turntable/photo-turntable/irRecv.cpp
@@ -1,6 +1,11 @@
 #include "IRremote.h"
 #include "IRremoteInt.h"
 
+#ifdef IR_TIMER_USE_ESP32
+hw_timer_t *timer;
+void IRTimer(); // defined in IRremote.cpp
+#endif
+
 //+=============================================================================
 // Decodes the received IR message
 // Returns 0 if no data ready, 1 if data ready.
@@ -15,75 +20,74 @@ int  IRrecv::decode (decode_results *results)
 
 	if (irparams.rcvstate != STATE_STOP)  return false ;
 
-#if DECODE_NEC
-	DBG_PRINTLN("Attempting NEC decode");
-	if (decodeNEC(results))  return true ;
-#endif
+// #if DECODE_NEC
+// 	DBG_PRINTLN("Attempting NEC decode");
+// 	if (decodeNEC(results))  return true ;
+// #endif
 
-#if DECODE_SONY
-	DBG_PRINTLN("Attempting Sony decode");
-	if (decodeSony(results))  return true ;
-#endif
+// #if DECODE_SONY
+// 	DBG_PRINTLN("Attempting Sony decode");
+// 	if (decodeSony(results))  return true ;
+// #endif
 
-#if DECODE_SANYO
-	DBG_PRINTLN("Attempting Sanyo decode");
-	if (decodeSanyo(results))  return true ;
-#endif
+// #if DECODE_SANYO
+// 	DBG_PRINTLN("Attempting Sanyo decode");
+// 	if (decodeSanyo(results))  return true ;
+// #endif
 
-#if DECODE_MITSUBISHI
-	DBG_PRINTLN("Attempting Mitsubishi decode");
-	if (decodeMitsubishi(results))  return true ;
-#endif
+// #if DECODE_MITSUBISHI
+// 	DBG_PRINTLN("Attempting Mitsubishi decode");
+// 	if (decodeMitsubishi(results))  return true ;
+// #endif
 
-#if DECODE_RC5
-	DBG_PRINTLN("Attempting RC5 decode");
-	if (decodeRC5(results))  return true ;
-#endif
+// #if DECODE_RC5
+// 	DBG_PRINTLN("Attempting RC5 decode");
+// 	if (decodeRC5(results))  return true ;
+// #endif
 
-#if DECODE_RC6
-	DBG_PRINTLN("Attempting RC6 decode");
-	if (decodeRC6(results))  return true ;
-#endif
+// #if DECODE_RC6
+// 	DBG_PRINTLN("Attempting RC6 decode");
+// 	if (decodeRC6(results))  return true ;
+// #endif
 
-#if DECODE_PANASONIC
-	DBG_PRINTLN("Attempting Panasonic decode");
-	if (decodePanasonic(results))  return true ;
-#endif
+// #if DECODE_PANASONIC
+// 	DBG_PRINTLN("Attempting Panasonic decode");
+// 	if (decodePanasonic(results))  return true ;
+// #endif
 
-#if DECODE_LG
-	DBG_PRINTLN("Attempting LG decode");
-	if (decodeLG(results))  return true ;
-#endif
+// #if DECODE_LG
+// 	DBG_PRINTLN("Attempting LG decode");
+// 	if (decodeLG(results))  return true ;
+// #endif
 
-#if DECODE_JVC
-	DBG_PRINTLN("Attempting JVC decode");
-	if (decodeJVC(results))  return true ;
-#endif
+// #if DECODE_JVC
+// 	DBG_PRINTLN("Attempting JVC decode");
+// 	if (decodeJVC(results))  return true ;
+// #endif
 
-#if DECODE_SAMSUNG
-	DBG_PRINTLN("Attempting SAMSUNG decode");
-	if (decodeSAMSUNG(results))  return true ;
-#endif
+// #if DECODE_SAMSUNG
+// 	DBG_PRINTLN("Attempting SAMSUNG decode");
+// 	if (decodeSAMSUNG(results))  return true ;
+// #endif
 
-#if DECODE_WHYNTER
-	DBG_PRINTLN("Attempting Whynter decode");
-	if (decodeWhynter(results))  return true ;
-#endif
+// #if DECODE_WHYNTER
+// 	DBG_PRINTLN("Attempting Whynter decode");
+// 	if (decodeWhynter(results))  return true ;
+// #endif
 
-#if DECODE_AIWA_RC_T501
-	DBG_PRINTLN("Attempting Aiwa RC-T501 decode");
-	if (decodeAiwaRCT501(results))  return true ;
-#endif
+// #if DECODE_AIWA_RC_T501
+// 	DBG_PRINTLN("Attempting Aiwa RC-T501 decode");
+// 	if (decodeAiwaRCT501(results))  return true ;
+// #endif
 
-#if DECODE_DENON
-	DBG_PRINTLN("Attempting Denon decode");
-	if (decodeDenon(results))  return true ;
-#endif
+// #if DECODE_DENON
+// 	DBG_PRINTLN("Attempting Denon decode");
+// 	if (decodeDenon(results))  return true ;
+// #endif
 
-#if DECODE_LEGO_PF
-	DBG_PRINTLN("Attempting Lego Power Functions");
-	if (decodeLegoPowerFunctions(results))  return true ;
-#endif
+// #if DECODE_LEGO_PF
+// 	DBG_PRINTLN("Attempting Lego Power Functions");
+// 	if (decodeLegoPowerFunctions(results))  return true ;
 
 	// decodeHash returns a hash on any input.
 	// Thus, it needs to be last in the list.
@@ -117,6 +121,17 @@ IRrecv::IRrecv (int recvpin, int blinkpin)
 //
 void  IRrecv::enableIRIn ( )
 {
+// Interrupt Service Routine - Fires every 50uS
+#ifdef ESP32
+	// ESP32 has a proper API to setup timers, no weird chip macros needed
+	// simply call the readable API versions :)
+	// 3 timers, choose #1, 80 divider nanosecond precision, 1 to count up
+	timer = timerBegin(1, 80, 1);
+	timerAttachInterrupt(timer, &IRTimer, 1);
+	// every 50ns, autoreload = true
+	timerAlarmWrite(timer, 50, true);
+	timerAlarmEnable(timer);
+#else
 	cli();
 	// Setup pulse clock timer interrupt
 	// Prescale /8 (16M/8 = 0.5 microseconds per tick)
@@ -130,6 +145,7 @@ void  IRrecv::enableIRIn ( )
 	TIMER_RESET;
 
 	sei();  // enable interrupts
+#endif
 
 	// Initialize state machine variables
 	irparams.rcvstate = STATE_IDLE;
--- photo-turntable/photo-turntable/photo-turntable.ino
+++ photo-turntable/photo-turntable/photo-turntable.ino
@@ -5,6 +5,7 @@
 #include "Encoder.h"
 #include "Button.h"
 #include "math.h"
+#include <Wire.h>
 
 // Pin Definitions
 #define RotaryEncoder_PIN_D	4
@@ -18,67 +19,166 @@
 
 // set default parameters
 int speed = 100;
-int angles = 4;
+int angles = 8;
 const int fullRotation = 18300;
-
-// Global variables and defines
-// There are several different versions of the LCD I2C adapter, each might have a different address.
-// Try the given addresses by Un/commenting the following rows until LCD works follow the serial monitor prints.
-// To find your LCD address go to: http://playground.arduino.cc/Main/I2cScanner and run example.
-#define LCD_ADDRESS 0x3F
-//#define LCD_ADDRESS 0x27
 
 // Define LCD characteristics
 #define LCD_ROWS 2
 #define LCD_COLUMNS 16
 #define SCROLL_DELAY 150
-#define BACKLIGHT 255
+#define ZURUECKLIGHT 255
 long rotaryEncDOldPosition  = 0;
+
 // object initialization
 Encoder rotaryEncD(RotaryEncoder_PIN_D, RotaryEncoder_PIN_CLK);
 Button rotaryEncDButton(RotaryEncoder_PIN_S1);
 LiquidCrystal_PCF8574 lcdI2C;
 IRsend ir_led;
 
-enum menuState {HOME, VIDEO, STILLS, VIDSTART, STILSTART, SPEED, ANGLES, VIDBACK, STILBACK, CHANGESPEED, CHANGEANGLES} state = VIDEO, oldState = 10;
+//IR Receiver
+#define RECV_PIN 8
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+enum menuState {START, VIDEO, FOTOS, VIDSTART, FOTOSTART, GESCHWINDIGKEIT, ANZAHL, VIDZURUECK, STILZURUECK, CHANGEGESCHWINDIGKEIT, CHANGEANZAHL} state = FOTOS, oldState = VIDEO;
+
+// Global variables and defines
+// There are several different versions of the LCD I2C adapter, each might have a different address.
+// Try the given addresses by Un/commenting the following rows until LCD works follow the serial monitor prints.
+// To find your LCD address go to: http://playground.arduino.cc/Main/I2cScanner and run example.
+//#define LCD_ADDRESS 0x3F
+//#define LCD_ADDRESS 0x27
+//aka #define LCD_ADDRESS 39
+//#define LCD_ADDRESS 0x23
 
 // Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
 void setup()
 {
+  delay(1500);
   // Setup Serial which is useful for debugging
   // Use the Serial Monitor to view printed messages
-  Serial.begin(9600);
-  while (!Serial) ; // wait for serial port to connect. Needed for native USB
-  Serial.println("start");
+  //Serial.begin(9600);
+  //while (!Serial) ; // wait for serial port to connect. Needed for native USB
+  //Serial.println("start");
+  byte error, address;
+  int nDevices;
 
+  //Serial.println("Scanning...");
+
+  nDevices = 0;
+  //  for (address = 1; address < 127; address++ )
+  for (address = 30; address < 50; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      //Serial.print("I2C device found at address ");
+      if (address < 16)
+      {
+        //Serial.print("0");
+      }
+      //Serial.print(address);
+      //Serial.println("!");
+
+      delay(2500);
+      //#define LCD_ADDRESS address
+      delay(50);
+      lcdI2C.begin(LCD_COLUMNS, LCD_ROWS, address, ZURUECKLIGHT);
+      delay(50);
+      lcdI2C.clear();
+      delay(50);
+      lcdI2C.print("Alles ready?");
+      delay(1000);
+      lcdI2C.print("");
+      lcdI2C.selectLine(2);
+      for (int i = 0 ; i < 17 ; i++)
+      {
+        lcdI2C.print("\333");
+        delay(600 - i * 35);
+      }
+      delay(250);
+
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      //Serial.print("Unknown error at address ");
+      if (address < 16)
+      {
+        //Serial.print("0");
+      }
+      //Serial.println(address);
+    }
+  }
+  if (nDevices == 0)
+  {
+    //Serial.println("No I2C devices found\n");
+  }
+  else
+  {
+    //Serial.println("done\n");
+  }
   // initialize the lcd
-  lcdI2C.begin(LCD_COLUMNS, LCD_ROWS, LCD_ADDRESS, BACKLIGHT);
   rotaryEncDButton.init();
   pinMode(RotaryEncoder_PIN_S1, INPUT_PULLUP);
 
-  lcdI2C.print("PHOTO BOOTH");
-  delay(500);
-  lcdI2C.print("");
-
   pinMode(STEPPER_PIN_STEP, OUTPUT);
+
+  //IR Receiver strart
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
 }
 
 // Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
 void loop()
 {
-  //Read encoder new position
-  long rotaryEncDNewPosition = rotaryEncD.read() / 2;
-  bool select = rotaryEncDButton.onPress();
   char menuDir = ' ';
+  bool select = rotaryEncDButton.onPress();
+
+  //IR Receiver actions
+  // playpause D7E84B1B 3622325019
+  // prievious 52A3D41F 1386468383
+  // down F076C13B 4034314555
+  // next 20FE4DBB 553536955
+  // up E5CFBD7F 3855596927
+  if (irrecv.decode(&results)) {
+    irrecv.resume();
+    if (results.value == 1386468383 or results.value == 4034314555)
+    {
+      menuDir = 'L';
+      delay(5);
+    }
+    if (results.value == 553536955 or results.value == 3855596927)
+    {
+      menuDir = 'R';
+      delay(5);
+    }
+    if (results.value == 3622325019)
+    {
+      select = true;
+      delay(5);
+    }
+  }
+
+
+  //Read encoder new position
+  long rotaryEncDNewPosition = rotaryEncD.read() / 4;
   if (rotaryEncDNewPosition != rotaryEncDOldPosition) {
     //Serial.println(rotaryEncDNewPosition);
     if (rotaryEncDNewPosition < rotaryEncDOldPosition)
     {
       menuDir = 'L';
+      delay(5);
     }
     else if (rotaryEncDNewPosition >= rotaryEncDOldPosition)
     {
       menuDir = 'R';
+      delay(5);
     }
     rotaryEncDOldPosition = rotaryEncDNewPosition;
   }
@@ -90,7 +190,6 @@ void loop()
     oldState = state;
   }
 
-
   lcdI2C.selectLine(1);
   lcdI2C.print(printState(state));
 
@@ -98,28 +197,28 @@ void loop()
   {
     case VIDEO:
       if (menuDir == 'L' || menuDir == 'R')
-        state = STILLS;
+        state = FOTOS;
 
       if (select)
         state = VIDSTART ;
 
       break;
 
-    case STILLS:
+    case FOTOS:
       if (menuDir == 'L' || menuDir == 'R')
         state = VIDEO;
 
       if (select)
-        state = STILSTART;
+        state = FOTOSTART;
 
       break;
 
     case VIDSTART:
       if (menuDir == 'R')
-        state = SPEED;
+        state = GESCHWINDIGKEIT;
 
       else if (menuDir == 'L')
-        state = VIDBACK;
+        state = VIDZURUECK;
 
       if (select) {
         video();
@@ -127,33 +226,33 @@ void loop()
       }
       break;
 
-    case SPEED:
+    case GESCHWINDIGKEIT:
       if (menuDir == 'L')
         state = VIDSTART;
 
       else if (menuDir == 'R')
-        state = VIDBACK;
+        state = VIDZURUECK;
 
       if (select)
 
-        state = CHANGESPEED;
+        state = CHANGEGESCHWINDIGKEIT;
 
       break;
 
-    case CHANGESPEED:
+    case CHANGEGESCHWINDIGKEIT:
       if (menuDir == 'L')
       {
-        speed-=5;
+        speed -= 5;
         lcdI2C.selectLine(2);
         lcdI2C.print("     ");
       }
       else if (menuDir == 'R')
       {
-        speed+=5;
+        speed += 5;
         lcdI2C.selectLine(2);
         lcdI2C.print("     ");
       }
-      speed=constrain(speed,0,100);
+      speed = constrain(speed, 0, 100);
       lcdI2C.selectLine(2);
       lcdI2C.print(speed);
       lcdI2C.print("%");
@@ -163,9 +262,9 @@ void loop()
         state = VIDSTART;
 
       break;
-    case VIDBACK:
+    case VIDZURUECK:
       if (menuDir == 'L')
-        state = SPEED;
+        state = GESCHWINDIGKEIT;
 
       else if (menuDir == 'R')
         state = VIDSTART;
@@ -177,31 +276,31 @@ void loop()
       break;
 
 
-    case STILSTART:
+    case FOTOSTART:
       if (menuDir == 'R')
-        state = ANGLES;
+        state = ANZAHL;
 
       else if (menuDir == 'L')
-        state = STILBACK;
+        state = STILZURUECK;
 
       if (select)
         stills();
       break;
 
-    case ANGLES:
+    case ANZAHL:
       if (menuDir == 'L')
-        state = STILSTART;
+        state = FOTOSTART;
 
       else if (menuDir == 'R')
-        state = STILBACK;
+        state = STILZURUECK;
 
 
       if (select)
-        state = CHANGEANGLES;
+        state = CHANGEANZAHL;
 
       break;
 
-    case CHANGEANGLES:
+    case CHANGEANZAHL:
       if (menuDir == 'L')
       {
         angles--;
@@ -219,18 +318,18 @@ void loop()
       lcdI2C.print(angles);
 
       if (select)
-        state = STILSTART;
+        state = FOTOSTART;
       break;
 
-    case STILBACK:
+    case STILZURUECK:
       if (menuDir == 'L')
-        state = ANGLES;
+        state = ANZAHL;
 
       else if (menuDir == 'R')
-        state = STILSTART;
+        state = FOTOSTART;
 
       if (select)
-        state = STILLS;
+        state = FOTOS;
       break;
   }
 
@@ -243,44 +342,65 @@ String printState(int curstate)
 {
   switch (curstate) {
     case 0:
-      return "HOME";
+      return "Start";
     case 1:
-      return "VIDEO";
+      return "Video";
     case 2:
-      return "STILLS";
+      return "Fotos";
     case 3:
-      return "VIDEO START";
+      return "Video starten";
     case 4:
-      return "STILLS START";
+      return "Fotos starten";
     case 5:
-      return "SET SPEED";
+      return "Geschwindigkeit";
     case 6:
-      return "SET ANGLES";
+      return "Anzahl Fotos";
     case 7:
-      return "BACK";
+      return "Zur\365ck";
     case 8:
-      return "BACK";
+      return "Zur\365ck";
     case 9:
-      return "SET SPEED:";
+      return "Geschwindigkeit:";
     case 10:
-      return "SET ANGLES:";
+      return "Anzahl Fotos:";
   }
 
 }
 
 
 void stills() {
-
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 3"));
+  delay(1000);
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 2"));
+  delay(1000);
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 1"));
+  delay(1000);
   for (int i = 0 ; i < angles ; i++) {
+    int imagesToTake = angles - i - 1;
     delay(300);
+    lcdI2C.clear();
     irStillsShot();
+    if (imagesToTake == 1)
+    {
+      lcdI2C.print(F("Noch "));
+      lcdI2C.print(imagesToTake);
+      lcdI2C.print(F(" Foto"));
+    }
+    else
+    {
+      lcdI2C.print(F("Noch "));
+      lcdI2C.print(imagesToTake);
+      lcdI2C.print(F(" Fotos"));
+    }
     delay(750);
     int delay_time = SLOW;
     int numOfSteps = fullRotation / angles;
     float accSteps = 600;
     for (int i = 0; i < numOfSteps; i++ )
     {
-
       if (i < accSteps)
       {
         delay_time = (SLOW - FAST) * pow(float(i - accSteps), 2) / float(accSteps * accSteps) + FAST;
@@ -306,15 +426,25 @@ void stills() {
 
 void video() {
   delay(300);
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 3"));
+  delay(1000);
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 2"));
+  delay(1000);
+  lcdI2C.clear();
+  lcdI2C.print(F("Start in 1"));
+  delay(1000);
+  lcdI2C.clear();
   irVideoShot();
+  lcdI2C.print(F("Video gestartet"));
   delay(750);
   int delay_time = SLOW;
-  int fast = 300 + 12*(100-speed);
+  int fast = 300 + 12 * (100 - speed);
   int numOfSteps = fullRotation;
   float accSteps = 600;
   for (int i = 0; i < numOfSteps; i++ )
   {
-
     if (i < accSteps)
     {
       delay_time = (SLOW - fast) * pow(float(i - accSteps), 2) / float(accSteps * accSteps) + fast;
@@ -334,27 +464,42 @@ void video() {
   }
   delay(750);
   irVideoShot();
-  delay(500);
-
+  lcdI2C.clear();
+  lcdI2C.print(F("Video beendet"));
+  delay(1200);
+  lcdI2C.clear();
 }
 
 
-
-
-
-void irVideoShot() {
-  int khz = 33; // 33kHz frequency
-  unsigned int irSignal[] = {488};
-  ir_led.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
-  delayMicroseconds(5360);
-  ir_led.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
-}
+//Sony IR-Codes:
+//Photo     740239    B4B8F   // Shutter | take photo
+//VID       76687     12B8F   // Start | stop video recording
+//DISP      166799    28B8F   // Display change cycles round
+//Menu      117647    1CB8F   // Enter menu | leave menu
+//MenuU     379791    5CB8F   // Menu up
+//menuD     904079    DCB8F   // Menu down
+//menuR     1035151   FCB8F   // Menu right
+//menuL     510863    7CB8F   // Menu left
+//OK        641937    9CB91   // Menu OK
+//Z+        338831    52B8F   // Zoom in
+//Z-        863119    D2B8F   // Zoom out
 
 void irStillsShot() {
-  int khz = 33; // 33kHz frequency
-  unsigned int irSignal[] = {480};
-  ir_led.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
-  delayMicroseconds(7330);
-  ir_led.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
+  for (int i = 0; i < 3; i++)
+  {
+    ir_led.sendSony(0xB4B8F, 20);
+    delay(40);
+    irrecv.enableIRIn();
+    irrecv.blink13(true);
+  }
 }
 
+void irVideoShot() {
+  for (int i = 0; i < 3; i++)
+  {
+    ir_led.sendSony(0x12B8F, 20);
+    delay(40);
+    irrecv.enableIRIn();
+    irrecv.blink13(true);
+  }
+}
